57baa35be5d43b610ad52192bb53c16d515553bb
Dustin E. Homan <dustin.e.homan@gmail.com>
2017-10-06 08:39:07 -0600

    Added read cat

--- src/controllers/support/support.controller.ts
+++ src/controllers/support/support.controller.ts
@@ -38,6 +38,21 @@ export class SupportController extends BaseController {
         }
     }
 
+    @Get('/category/:name')
+    public async readCategory( @Response() res, @Session() session, @Param('name') catName, @Headers('x-force-refresh') refresh = 'false'): Promise<Response> {
+        let role = 'Anonymous';
+        if (session.user && session.user.role)
+            role = session.user.role.name + "s";
+
+        try {
+            let pages = await this.supportService.getAll(role, refresh === 'true');
+            pages = pages.filter(page => page.Category__c === catName);
+            return res.status(HttpStatus.OK).json(pages);
+        } catch (error) {
+            return this.handleError(res, 'Error in SupportController.readCategory(): ', error);
+        }
+    }
+
     @Get('/:id')
     public async read( @Response() res, @Session() session, @Param('id') id, @Headers('x-force-refresh') refresh = 'false'): Promise<Response> {
         let role = 'Anonymous';
