d311e439c5cc1ea33a7abe5aeaacc76084ed2981
Abraham White <abelincoln.white@gmail.com>
2018-07-27 17:42:54 -0600

    Update Dockerfile and run script

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,15 @@ FROM node:8.9-alpine
 ENV NODE_ENV production
 WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
-RUN npm install --production --silent && mv node_modules ../
+RUN npm install --production --silent
 COPY . .
+RUN npm run build
+EXPOSE 80
 ENV PORT 80
 ENV LOG_PATH ./
-ENV GLOBAL_PREFIX /v2/affiliates
 ENV LOG_FILE shingo-affiliates-api.log
+# the following may be overriden by run script
 ENV LOG_LEVEL info
+ENV GLOBAL_PREFIX /v2/affiliates
 ENV CLIENT_HOST https://affiliates.shingo.org
-RUN npm build
-EXPOSE 80
 CMD npm start
--- run.sh
+++ run.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-
-TAG="${TAG:-latest}"
-NETWORK="${NETWORK:-shingo-net}"
 IMG_NAME="shingo-affiliates-api"
 CONT_NAME="shingo-affiliates-api"
-PORT=${PORT:-80}
-SF_API=${SF_API:-shingo-sf-api}
-AUTH_API=${AUTH_API:-shingo-auth-api}
-SHINGO_REDIS=${SHINGO_REDIS:-shingo-redis}
 
-EMAIL_PASS=${EMAIL_PASS:?"Must set EMAIL_PASS"}
+if [[ "$MODE" == "dev" ]]; then
+  PUBLISH_PORT="${PORT:-8080}"
+  TAG="${TAG:-local}"
+  NETWORK="${NETWORK:-shingo-dev-net}"
+  export NODE_ENV="${NODE_ENV:-development}"
+  export GLOBAL_PREFIX="/"
+  export LOG_LEVEL="debug"
+  export CLIENT_HOST="http://localhost:$PORT"
+else
+  TAG="${TAG:-latest}"
+  NETWORK="${NETWORK:-shingo-net}"
+  export SHINGO_REDIS="${SHINGO_REDIS:-shingo-redis}"
+  export EMAIL_PASS="${EMAIL_PASS:?Must set EMAIL_PASS}"
+fi
+
+export SF_API="${SF_API:-shingo-sf-api}"
+export AUTH_API="${AUTH_API:-shingo-auth-api}"
 
 if [[ "$TAG" = "test" ]]; then
   CONT_NAME+="-test"
@@ -17,12 +26,22 @@ fi
 
 NAME="${NAME:-$CONT_NAME}"
 
-docker run -itd                     \
-    --name "$NAME"                  \
-    --network "$NETWORK"            \
-    --publish "$PORT":80            \
-    -e EMAIL_PASS="$EMAIL_PASS"     \
-    -e SF_API="$SF_API"             \
-    -e AUTH_API="$AUTH_API"         \
-    -e SHINGO_REDIS="$SHINGO_REDIS" \
-    docker.shingo.org/"$IMG_NAME":"$TAG"
+args=(
+  -itd
+  --name "$NAME"
+  --network "$NETWORK"
+  -e EMAIL_PASS
+  -e SF_API
+  -e AUTH_API
+  -e SHINGO_REDIS
+  -e NODE_ENV
+  -e GLOBAL_PREFIX
+  -e LOG_LEVEL
+  -e CLIENT_HOST
+)
+
+if [[ ! -z "$PUBLISH_PORT" ]]; then
+  args+=(-p "$PUBLISH_PORT":80)
+fi
+
+docker run "${args[@]}" docker.shingo.org/"$IMG_NAME":"$TAG"
