afd23bbaa56a8c24f1a2fd86ed85abf481afc233
Abraham White <abelincoln.white@gmail.com>
2018-08-30 15:27:44 -0600

    Clean up user component

--- src/components/user/user.component.ts
+++ src/components/user/user.component.ts
@@ -2,27 +2,23 @@ import { Injectable } from '@nestjs/common';
 export { User } from './user';
 
 /**
- * @desc Provide methods for working with users
- * 
- * @export
- * @class UserService
+ * Provide methods for working with users
  */
 @Injectable()
 export class UserService {
 
-    /**
-     * @desc Parse out the workshops that a user has permissions for
-     * 
-     * @param {any} user - Requires <code>user.permissions[]</code> and <code>user.roles[].permissions[]</code>
-     * @returns {string[]} 
-     * @memberof UserService
-     */
-    public getWorkshopIds(user: { permissions: Array<{ resource: string }>, role: { permissions: Array<{resource: string}> } }): string[] {
-        const ids = [...user.permissions, ...user.role.permissions]
-                .filter(p => p.resource.includes('/workshops/'))
-                .map(p => `'${p.resource.replace('/worshops/', '')}'`)
+  /**
+   * Parse out the workshops that a user has permissions for
+   *
+   * @param user Requires user.permissions[] and user.roles[].permissions[]
+   */
+  // tslint:disable-next-line:max-line-length
+  getWorkshopIds(user: { permissions: Array<{ resource: string }>, roles: { permissions: Array<{resource: string}> } }): string[] {
+    const ids =
+      [...user.permissions, ...user.roles.permissions]
+          .filter(p => p.resource.includes('/workshops/'))
+          .map(p => `'${p.resource.replace('/worshops/', '')}'`)
 
-        return [...new Set(ids)]; // Only return unique ids
-    }
-
+    return [...new Set(ids)]; // Only return unique ids
+  }
 }
